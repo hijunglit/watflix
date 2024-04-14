f1c32309bbb92ee01cd9d2f93bfe5b0df0c7c3ae
hijunglit <hijunglit@gmail.com>
2024-04-15 00:12:50 +0900

    motion value, useTransform (framer motion)

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
 import styled from "styled-components";
+import {
+  motion,
+  motionValue,
+  useMotionValue,
+  useTransform,
+} from "framer-motion";
 
 function App() {
   const Wrapper = styled.div`
+    width: 100vh;
     height: 100vh;
-    width: 100vw;
     display: flex;
     justify-content: center;
     align-items: center;
   `;
-  const Box = styled.div`
+  const Box = styled(motion.ul)`
     width: 200px;
     height: 200px;
-    background-color: white;
-    border-radius: 10px;
+    background-color: rgba(255, 255, 255, 1);
+    border-radius: 40px;
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
   `;
+  const x = useMotionValue(0);
+  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
   return (
     <Wrapper>
-      <Box />
+      <Box style={{ x, scale }} drag='x' dragSnapToOrigin />
     </Wrapper>
   );
 }
